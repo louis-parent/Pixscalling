f173b40fc54c7fe0c82850a4d801c4000ffd5a72
H4KRO <32486404+H4KRO@users.noreply.github.com>
2018-09-21 21:58:32 +0200

    Time counter

--- bmp.c
+++ bmp.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() {
+#define INPUT_FILE "in.bmp"
+
+int convert1(){
     FILE *in;
     FILE *out;
 
-    in = fopen("in.bmp", "rb");
+    in = fopen(INPUT_FILE, "rb");
     out = fopen("out.bmp", "wb");
 
     fseek(in, 0L, SEEK_END);
     int size = ftell(in);
     fseek(in, 0L, SEEK_SET);
 
-   for(int i = 0; i < size; i++){
-       if(i < 34){
-           fputc(fgetc(in), out);
-       }else{
-           fputc(fgetc(in)+19000000, out);
-       }
+    for(int i = 0; i < size; i++){
+        if(i < 34){
+            fputc(fgetc(in), out);
+        }else{
+            fputc(fgetc(in)+19000000, out);
+        }
     }
 
     fclose(in);
     fclose(out);
 }
+
+int convert2(){
+
+}
+
+int main() {
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
+    convert1();
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("%f\n", cpu_time_used);
+}
+
