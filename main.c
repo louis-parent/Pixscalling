14da020f680fad0f673a4ae41fd33071521f1cdf
H4KRO <eddie.vallier@gmail.com>
2018-09-26 16:01:47 +0200

    Creating bmp to int convertion

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define INPUT_FILE "in.bmp"
 
-int convert1(){
+void convert1(){
     FILE *in;
     FILE *out;
 
@@ -16,10 +17,11 @@ int convert1(){
     fseek(in, 0L, SEEK_SET);
 
     for(int i = 0; i < size; i++){
+        char c = fgetc(in);
         if(i < 34){
-            fputc(fgetc(in), out);
+            fputc(c, out);
         }else{
-            fputc(fgetc(in)+19000000, out);
+            fputc(c, out);
         }
     }
 
@@ -27,22 +29,81 @@ int convert1(){
     fclose(out);
 }
 
-int convert2(){
+void bmpToInt(){
+    FILE *in;
+    FILE *out;
 
+    in = fopen(INPUT_FILE, "rb");
+    out = fopen("out1.bmp", "wb");
+
+    fseek(in, 0L, SEEK_END);
+    int size = ftell(in);
+    fseek(in, 0L, SEEK_SET);
+
+    for(int i = 0; i < size; i++){
+        char c = fgetc(in);
+        fprintf(out, "%i\n", c);
+    }
+
+    fclose(in);
+    fclose(out);
 }
 
-int main() {
+void intToBmp(){
+    FILE *in;
+    FILE *out;
 
+    in = fopen("out1.bmp", "rb");
+    out = fopen("out2.bmp", "wb");
+
+    fseek(in, 0L, SEEK_END);
+    int size = ftell(in);
+    fseek(in, 0L, SEEK_SET);
+
+    int count = 0;
+    char c;
+
+    while((c = fgetc(in)) != EOF){
+        char buffer[4];
+        do{
+            buffer[count] = c;
+            count++;
+        }while((c = fgetc(in)) != '\n');
+        buffer[count] = ' ';
+        fprintf(out, "%c", (signed int) atoi(buffer));
+        count = 0;
+    }
+
+    fclose(in);
+    fclose(out);
+}
+
+
+
+double timeCounter(void (*ptr)()){
     clock_t start, end;
-    double cpu_time_used;
 
     start = clock();
 
-    convert1();
+    (*ptr)();
 
     end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-    printf("%f\n", cpu_time_used);
+    return ((double) (end - start)) / CLOCKS_PER_SEC;
 }
+
+double timeCounterAverage(void (*ptr)(), int n){
+    double sum = .0;
+    for(int i = 0; i < n; i++){
+        sum += timeCounter(ptr);
+    }
+    return sum/n;
+    
+}
+
+int main() {
+    bmpToInt();
+    intToBmp();
+}
+
+
 
