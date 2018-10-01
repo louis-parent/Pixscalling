e5ad67b9c70f0b29eaac2781a68717cd293f6628
falentheo <theoarmand@hotmail.fr>
2018-10-01 18:00:21 +0200

    Progression of first implementation

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 #include "headers/ppm.h"
 #include "headers/pixel.h"
+#include "headers/matrix.h"
 #include "headers/timer.h"
 
 #define INPUT_FILE "in.ppm"
@@ -101,7 +102,40 @@ void upscale(){
 	growup(5);
 }
 
-void test()
+Pixel* arroUp(Matrix* matrice, int x, int y){
+	if(y <= 0){
+		return select(matrice, x, y);
+	}
+	return select(matrice, x, y-1);
+}
+
+Pixel* arroDown(Matrix* matrice, int x, int y){
+	return select(matrice, x, y+1);
+}
+
+Pixel* arroLeft(Matrix* matrice, int x, int y){
+	if(x <= 0){
+		return select(matrice, x, y);
+	}
+	return select(matrice, x-1, y);
+}
+
+Pixel* arroRight(Matrix* matrice, int x, int y){
+	if(x >= getWidth(matrice)){
+		return select(matrice, x, y);
+	}
+	return select(matrice, x+1, y);
+}
+
+int comparePixel(Pixel* a, Pixel* b){
+	return getRed(a) == getRed(b) && getGreen(a) == getGreen(b) && getBlue(a) == getBlue(b);
+}
+
+
+
+
+
+	void test()
 {
 	printf("Begining Test :\n");
     printf("\t- Simple Copy Test : %f\n", timeCounterAverage(copyFile, 1));
@@ -113,7 +147,87 @@ void test()
 
 int main()
 {
-	//test();
-	upscale();
+	int mult = 2;
+	PPM* inPPM = readPPM(INPUT_FILE);
+	Matrix* in = getContent(inPPM);
+
+	int inWidth = getWidth(inPPM);
+	int inHeight = getHeight(inPPM);
+
+	Matrix* out = createMatrix(inWidth * mult, inHeight * mult);
+
+	for(int y = 0; y < inHeight; y++){
+		for(int x = 0; x < inWidth; x++){
+
+			set(out, 2*x, 2*y, copyPixel(getPixel(inPPM, x, y)));
+			set(out, 2*x+1, 2*y, copyPixel(getPixel(inPPM, x, y)));
+			set(out, 2*x, 2*y+1, copyPixel(getPixel(inPPM, x, y)));
+			set(out, 2*x+1, 2*y+1, copyPixel(getPixel(inPPM, x, y)));
+
+			// UP
+			Pixel* up = select(in, x, y);
+			if(y > 0){
+				up = select(in, x, y-1);
+			}
+
+			// Down
+			Pixel* down = select(in, x, y);
+			if(y < inWidth-1){
+				down = select(in, x, y+1);
+			}
+
+			// Left
+			Pixel* left = select(in, x, y);
+			if(x > 0){
+				left = select(in, x-1, y);
+			}
+
+			// Right
+			Pixel* right = select(in, x, y);
+			if(x < inHeight-1){
+				right = select(in, x+1, y);
+			}
+
+
+			if(comparePixel(up, left)){
+				set(out, 2*x, 2*y, up);
+			}
+
+			if(comparePixel(up, right)){
+				set(out, 2*x+1, 2*y, up);
+			}
+/* A CORRIGER !!!
+			if(comparePixel(down, left) && y+1 < inHeight){
+				set(out, 2*x, 2*y+1, left);
+			}
+
+			if(comparePixel(down, right)){
+				set(out, 2*x+1, 2*y+1, down);
+			}
+*/
+			//set(out, mult*x+j, mult*y+i, copyPixel(getPixel(inPPM, x, y)));
+				
+			
+		}
+
+	}
+
+	PPM* outPPM = malloc(sizeof(PPM));
+	initPPM(outPPM);
+	setWidth(outPPM, inWidth*mult);
+	setHeight(outPPM, inHeight*mult);
+	setContent(outPPM, out);
+	writePPM(OUTPUT_FILE, outPPM);
+	
+	removePPM(inPPM);
+	removePPM(outPPM);
+
     return EXIT_SUCCESS;
 }
+
+/*
+					Pixel* up = arroUp(in, x, y);
+					Pixel* down = arroDown(in, x, y);
+					Pixel* left = arroLeft(in, x, y);
+					Pixel* right = arroRight(in, x, y);
+*/
