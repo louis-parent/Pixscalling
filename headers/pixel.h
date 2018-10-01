5444afa88746f9551b4eee9de90713691900e7c5
Appixroid <louisparent66@yahoo.fr>
2018-10-01 16:24:08 +0200

    Finishing commenting all the files

--- headers/pixel.h
+++ headers/pixel.h
@@ -1,30 +1,109 @@
 #ifndef H_PIXEL
 #define H_PIXEL
 
+/**
+ * Structure representing a Pixel with its three color values
+ */
 typedef struct Pixel Pixel;
 struct Pixel
 {
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
+	unsigned char r;// The value of the red color
+	unsigned char g;// The value of the green color
+	unsigned char b;// The value of the blue color
 };
 
+/**
+ * Initialize a Pixel to its default value
+ * @param Pixel* pixel : the Pixel to be initialized
+ */
 void initPixel(Pixel* pixel);
+
+/**
+ * Create a pixel from a RGB color
+ * @param unsigned char r : the red color value
+ * @param unsigned char g : the green color value
+ * @param unsigned char b : the blue color value
+ *
+ * @return the newly created Pixel
+ */
 Pixel* getRGB(unsigned char r, unsigned char g, unsigned char b);
 
+/**
+ * Retrieve the Red value of the given Pixel
+ * @param Pixel* pixel : the Pixel to be analyzed
+ *
+ * @return the red color value
+ */
 unsigned char getRed(Pixel* pixel);
+
+/**
+ * Retrieve the Green value of the given Pixel
+ * @param Pixel* pixel : the Pixel to be analyzed
+ *
+ * @return the green color value
+ */
 unsigned char getGreen(Pixel* pixel);
+
+/**
+ * Retrieve the Blue value of the given Pixel
+ * @param Pixel* pixel : the Pixel to be analyzed
+ *
+ * @return the blue color value
+ */
 unsigned char getBlue(Pixel* pixel);
 
+/**
+ * Change the color values of the given Pixel
+ * @param unsigned char r : the new red value
+ * @param unsigned char g : the new green value
+ * @param unsigned char b : the new blue value
+ */
 void modifyPixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b);
 
+/**
+ * Increase the color values of the given Pixel
+ * @param unsigned char r : the increment red value
+ * @param unsigned char g : the increment green value
+ * @param unsigned char b : the increment blue value
+ */
 void increasePixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b);
+
+/**
+ * Substract the color values of the given Pixel
+ * @param unsigned char r : the red value to substract
+ * @param unsigned char g : the green value to substract
+ * @param unsigned char b : the blue value to substract
+ */
 void substractPixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b);
+
+/**
+ * Multiply the color values of the given Pixel
+ * @param unsigned char r : the red value factor
+ * @param unsigned char g : the green value factor
+ * @param unsigned char b : the blue value factor
+ */
 void multiplyPixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b);
+
+/**
+ * Divide the color values of the given Pixel
+ * @param unsigned char r : the red value to divide by
+ * @param unsigned char g : the green value to divide by
+ * @param unsigned char b : the blue value to divide by
+ */
 void dividePixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b);
 
+/**
+ * Create a new Pixel with the same color as the given one
+ * @param Pixel* pixel : the Pixel to be copied
+ *
+ * @return the newly created Pixel
+ */
 Pixel* copyPixel(Pixel* pixel);
 
+/**
+ * Remove the Pixel from the memory
+ * @param Pixel* pixel : the Pixel to be deleted
+ */
 void removePixel(Pixel* pixel);
 
 #endif
--- sources/pixel.c
+++ sources/pixel.c
@@ -3,78 +3,78 @@
 
 void initPixel(Pixel* pixel)
 {
-	pixel->r = 0;
-	pixel->g = 0;
-	pixel->b = 0;
+	pixel->r = 255;// By default all the value are set to 255 (Color white)
+	pixel->g = 255;
+	pixel->b = 255;
 }
 
 Pixel* getRGB(unsigned char r, unsigned char g, unsigned char b)
 {
-	Pixel* pixel = malloc(3 * sizeof(unsigned char));
-	initPixel(pixel);
-	pixel->r = r;
-	pixel->g = g;
-	pixel->b = b;
+	Pixel* pixel = malloc(3 * sizeof(unsigned char));// Allocate the memory for a Pixel
+	initPixel(pixel);// Init a pixel to its default value
+	pixel->r = r;// Change the red value to the given one
+	pixel->g = g;// Change the green value to the given one
+	pixel->b = b;// Change the blue value to the given one
 
 	return pixel;
 }
 
 unsigned char getRed(Pixel* pixel)
 {
-	return pixel->r;
+	return pixel->r;// Return the red value of the Pixel
 }
 
 unsigned char getGreen(Pixel* pixel)
 {
-	return pixel->g;
+	return pixel->g;// Return the green value of the Pixel
 }
 
 unsigned char getBlue(Pixel* pixel)
 {
-	return pixel->b;
+	return pixel->b;// Return the blue value of the Pixel
 }
 
 void modifyPixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b)
 {
-	pixel->r = r;
+	pixel->r = r;// Set the value of each color to the given one
 	pixel->g = g;
 	pixel->b = b;
 }
 
 void increasePixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b)
 {
-	pixel->r += r;
+	pixel->r += r;// Increase the value of each color with the given increment
 	pixel->g += g;
 	pixel->b += b;
 }
 
 void substractPixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b)
 {
-	pixel->r -= r;
+	pixel->r -= r;// Substract the value of each color with the given value
 	pixel->g -= g;
 	pixel->b -= b;
 }
 
 void multiplyPixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b)
 {
-	pixel->r *= r;
+	pixel->r *= r;// Multiply the value of each color with the given factor
 	pixel->g *= g;
 	pixel->b *= b;
 }
 
 void dividePixel(Pixel* pixel, unsigned char r, unsigned char g, unsigned char b)
 {
-	pixel->r /= r;
+	pixel->r /= r;// Divide the value of each color with the given value
 	pixel->g /= g;
 	pixel->b /= b;
 }
 
 Pixel* copyPixel(Pixel* pixel)
 {
-	return getRGB(getRed(pixel), getGreen(pixel), getBlue(pixel));
+	return getRGB(getRed(pixel), getGreen(pixel), getBlue(pixel));// Create a Pixel with the value of the color of the given Pixel
 }
 
 void removePixel(Pixel* pixel)
 {
-	free(pixel);
+	free(pixel);//Free the pixel pointer
 }
