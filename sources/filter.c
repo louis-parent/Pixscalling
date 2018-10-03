bf4005a35273d4eb3f9ce73d66276d5fa9243ef5
H4KRO <eddie.vallier@gmail.com>
2018-10-03 11:06:51 +0200

    Second algorithm for first filter

--- sources/filter.c
+++ sources/filter.c
@@ -59,7 +59,7 @@ Matrix* growup(Matrix* matrix){
 	return out;
 }
 
-Matrix* scale2x(Matrix* matrix) {
+Matrix* scale2xOLD(Matrix* matrix) {
 	int mult = 2;
 
 	int inWidth = matrixColumns(matrix);
@@ -112,6 +112,82 @@ Matrix* scale2x(Matrix* matrix) {
 			}
 
 			if(comparePixel(down, right)){
+				set(out, 2*x+1, 2*y+1, copyPixel(down));
+			}
+		}
+	}
+	return out;
+}
+
+Matrix* scale2x(Matrix* matrix) {
+	int mult = 2;
+
+	int inWidth = matrixColumns(matrix);
+	int inHeight = matrixLines(matrix);
+
+	Matrix* out = createMatrix(inWidth * mult, inHeight * mult);
+
+	for(int y = 0; y < inHeight; y++){
+		for(int x = 0; x < inWidth; x++){
+			
+			set(out, 2*x, 2*y, copyPixel(select(matrix, x, y)));
+			set(out, 2*x+1, 2*y, copyPixel(select(matrix, x, y)));
+			set(out, 2*x, 2*y+1, copyPixel(select(matrix, x, y)));
+			set(out, 2*x+1, 2*y+1, copyPixel(select(matrix, x, y)));
+
+			// UP
+			Pixel* up = select(matrix, x, y);
+			if(y > 0){
+				up = select(matrix, x, y-1);
+			}
+
+			// Down
+			Pixel* down = select(matrix, x, y);
+			if(y < inHeight-1){
+				down = select(matrix, x, y+1);
+			}
+
+			// Left
+			Pixel* left = select(matrix, x, y);
+			if(x > 0){
+				left = select(matrix, x-1, y);
+			}
+			
+			// Right
+			Pixel* right = select(matrix, x, y);
+			if(x < inWidth-1){
+				right = select(matrix, x+1, y);
+			}
+/*
+			if(comparePixel(up, left)){
+				set(out, 2*x, 2*y, copyPixel(up));
+			}
+
+			if(comparePixel(up, right)){
+				set(out, 2*x+1, 2*y, copyPixel(up));
+			}
+
+			if(comparePixel(down, left) && y+1 < inHeight){
+				set(out, 2*x, 2*y+1, copyPixel(left));
+			}
+
+			if(comparePixel(down, right)){
+				set(out, 2*x+1, 2*y+1, copyPixel(down));
+			}
+*/
+			if(comparePixel(left, up) && !comparePixel(left, down) && !comparePixel(up, right)){
+				set(out, 2*x, 2*y, copyPixel(up));
+			}
+
+			if(comparePixel(up, right) && !comparePixel(up, left) && !comparePixel(right, down)){
+				set(out, 2*x+1, 2*y, copyPixel(right));
+			}
+
+			if(comparePixel(down, left) && !comparePixel(down, right) && !comparePixel(left, up)){
+				set(out, 2*x, 2*y+1, copyPixel(left));
+			}
+
+			if(comparePixel(right, down) && !comparePixel(right, up) && !comparePixel(left, down)){
 				set(out, 2*x+1, 2*y+1, copyPixel(down));
 			}
 		}
